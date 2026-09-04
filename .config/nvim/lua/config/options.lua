06d9902638c42ffa6b2d75e558761d62e6672833
Robert Nystrom <golgafrincham@gmail.com>
2026-09-04 16:42:39 +0200

    Force bash filetype for .bashrc in nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,8 @@ require("config.remote_clipboard").setup()
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- Treat .bashrc as bash, not POSIX sh. Otherwise the shell LSP (shuck) parses
+-- it as sh, rejects [[ ]], and floods the buffer with false "code is
+-- unreachable" / "no matching opener" diagnostics.
+vim.filetype.add({ filename = { [".bashrc"] = "bash" } })
